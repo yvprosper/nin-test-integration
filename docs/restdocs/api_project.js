9ac52b06a25673cda0050f979134e2298a0ede42
Ehichioya Prior Famous <fehichioya@gmail.com>
2021-09-03 13:06:30 +0100

    feat: update config

--- docs/restdocs/api_project.js
+++ docs/restdocs/api_project.js
@@ -1,15 +1,15 @@
 define({
-  "name": "User Service",
+  "name": "Raw Template Service",
   "version": "1.0.0",
-  "description": "apiDoc for the User Service",
-  "title": "User Service API Documentation",
-  "url": "http://127.0.0.1:30027/v1",
-  "sampleUrl": "http://127.0.0.1:30027/v1",
+  "description": "apiDoc for the Raw Template Service",
+  "title": "Raw Template Service API Documentation",
+  "url": "http://127.0.0.1:30041/v1",
+  "sampleUrl": "http://127.0.0.1:30042/v1",
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-04-25T11:09:35.651Z",
+    "time": "2021-09-03T12:05:53.095Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
